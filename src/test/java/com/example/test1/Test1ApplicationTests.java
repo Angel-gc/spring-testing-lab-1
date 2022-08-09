7e4d5cc990ced48199c15d1ca192065ef3f5d7ce
Angel Concepcion <angelconcepcion50@gmail.com>
2022-08-09 11:38:03 -0400

    a

--- src/test/java/com/example/test1/Test1ApplicationTests.java
+++ src/test/java/com/example/test1/Test1ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example2.demo;
+package com.example.test1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Test1ApplicationTests {
 
 	@Test
 	void contextLoads() {
